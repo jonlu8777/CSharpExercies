122c6e37797fc40d05afbf96cb9e658298845e6b
Jonas Lundberg <jonlu8777@gmail.com>
2022-11-01 16:31:13 +0100

    övningEmplyeeRegister

---
+++ ExOne/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExOne
+{
+    internal class Employee
+    {
+        public int employeeID;
+        public string employeeName;
+        public double employeeWage;
+
+        public Employee(int employeeID,string employeeName, double employeeWage)
+        {
+            this.employeeID = employeeID;
+            this.employeeName = employeeName;
+            this.employeeWage = employeeWage;
+        }
+    }
+}
--- ExOne/Program.cs
+++ ExOne/Program.cs
@@ -4,8 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, How many number of employees do you want to register:");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+            Employee[] employeeRegister = new Employee[numberOfEmployees];
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine("Enter name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter wage:");
+                double wage = double.Parse(Console.ReadLine());
+
+                employeeRegister[i] = new Employee(i, name, wage);
+                Console.WriteLine($"\nEmployee number {i+1} saved!");
+
+            }
+            Console.WriteLine("Registration complete!");
+
+            Console.WriteLine("These employees are saved:");
+            foreach (Employee employee in employeeRegister) 
+            {
+                Console.WriteLine();
+                Console.WriteLine("ID:" + employee.employeeID);
+                Console.WriteLine(employee.employeeName);
+                Console.WriteLine(employee.employeeWage);
+             
+             }
+
+            Console.WriteLine("Pick what ID number to view from above:");
+            int id = int.Parse(Console.ReadLine());
+
+            foreach(Employee employee in employeeRegister)
+            {
+                if(employee.employeeID == id)
+                {
+                    Console.WriteLine("ID:" + employee.employeeID);
+                    Console.WriteLine(employee.employeeName);
+                    Console.WriteLine(employee.employeeWage);
+                }
+            
+            
+            }
+            
+
+            
         }
     }
 }
